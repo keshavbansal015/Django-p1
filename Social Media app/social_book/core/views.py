3a638282569cc9a88feaf2c9efa4986ac02f8036
KeshavML <keshavbansalml@gmail.com>
2023-05-30 19:29:13 -0500

    Added static files

--- Social Media app/social_book/core/views.py
+++ Social Media app/social_book/core/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Welcome to Social Book.</h1>')
-
+    return render(request, 'index.html')
 
 
