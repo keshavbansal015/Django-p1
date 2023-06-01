d492b1c585f182211842d906abb79ad3d34fdce5
KeshavML <keshavbansalml@gmail.com>
2023-06-01 15:47:44 -0500

    Updated Upload pics

--- Social Media app/social_book/core/models.py
+++ Social Media app/social_book/core/models.py
@@ -7,14 +7,14 @@ User = get_user_model()
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user = models.IntegerField()
-    bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images',default='blank-profile-picture.png')
-    location = models.CharField(max_length=100, blank=True)
+    user        = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user     = models.IntegerField()
+    bio         = models.TextField(blank=True)
+    profileimg  = models.ImageField(upload_to='profile_images',default='blank-profile-picture.png')
+    location    = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user.usernames
 
 class Post(models.Model):
     id          = models.UUIDField(primary_key=True, default=uuid.uuid4)
--- Social Media app/social_book/core/views.py
+++ Social Media app/social_book/core/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_obj = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_obj)
+    return render(request, 'index.html',{'user_profile':user_profile})
 
 def signup(request):
     if request.method == "POST":
@@ -91,6 +93,16 @@ def settings(request):
 
 @login_required(login_url='signin')
 def upload(request):
+    if request.method == 'POST':
+        user    = request.user.username
+        image   = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+    else:
+        return redirect('/')
+
     return HttpResponse('<h1>Upload View</h1>')
 
 
