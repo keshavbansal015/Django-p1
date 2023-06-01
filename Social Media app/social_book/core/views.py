52e7a47311a613f6e2ceb8f6654c376652976514
KeshavML <keshavbansalml@gmail.com>
2023-06-01 13:48:10 -0500

    Added Sign up feature basic

--- Social Media app/social_book/core/views.py
+++ Social Media app/social_book/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from .models import Profile
 
 
 # Create your views here.
@@ -25,6 +26,13 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username,email=email,password=password)
                 user.save()
+                # log userin and direct to settings page
+
+                # create Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
         else:
             messages.info(request, 'Password not matching')
             return redirect('signup')
